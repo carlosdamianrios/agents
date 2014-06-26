2fa4e405e0f836a1b642181dcc6d6ab7ff59add8
carlosdamianrios <carlos.damian.rios@gmail.com>
2014-06-26 05:56:49 -0300

    POM master del proyecto

---
+++ src/main/java/ar/org/cdr/agents/App.java
@@ -0,0 +1,13 @@
+package ar.org.cdr.agents;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
